cd63ab6b5c4f973dd3f612009a59dde4a3bf7764
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-07-16 15:23:00 -0400

    Update UI.py
    Blurry more pixels de-res for antialiasing, but blurry no work right but it look cool also lag :(

--- UI.py
+++ UI.py
@@ -6,9 +6,10 @@ pygame.init()
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-screen_width = 700
-screen_height = 800
-dimensions = (28, 28)
+screen_width = 672
+screen_height = 772
+dimensions = (84, 84)
+blurred = False
 
 SCREEN = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('super duper cool awesomesauce ai')
@@ -19,7 +20,6 @@ clock = pygame.time.Clock()
 
 grid = {(x, y): 0.0 for x in range(dimensions[0]) for y in range(dimensions[1])}
 
-
 def drawGrid(w, h, d=dimensions):
     size = (int(w / d[0]), int(h / d[1]))
     for x in range(0, w, size[0]):
@@ -29,18 +29,23 @@ def drawGrid(w, h, d=dimensions):
 
 
 def setBox(p, color, d=dimensions, w=screen_width, h=screen_height-100):
-    boxx, boxy = mousePosToGrid(p)
-    if color != BLACK:
-        if grid[(boxx, boxy)] == 1:
-            return None
-        grid[(boxx, boxy)] = 1
-        antiAlias(p)
-        colorGrid()
-    else:
-        #erase
-        grid[(boxx, boxy)] = 0
-        colorGrid()
-    # drawGrid(w, h)
+    boxx_og, boxy_og = mousePosToGrid(p)
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            boxx, boxy = (boxx_og+x, boxy_og + y)
+            #print(boxx, boxy)
+            if (boxx, boxy) not in grid:
+                continue
+            if color != BLACK:
+                if grid[(boxx, boxy)] == 1:
+                    continue
+                grid[(boxx, boxy)] = 1
+                #antiAlias(p)
+                colorGrid()
+            else:
+                #erase
+                grid[(boxx, boxy)] = 0
+                drawGrid(w, h)
 
 def mousePosToGrid(pos):
     x, y = pos
@@ -66,16 +71,35 @@ def antiAlias(pos):
 def colorGrid():
     for key, value in grid.items():
         x, y = key
-        size = (int(screen_width / 28), int((screen_height-100) / 28))
+        size = (int(screen_width / dimensions[0]), int((screen_height-100) / dimensions[1]))
         bg_rect = pygame.Rect(x * size[0], y * size[1], size[0], size[1])
         rect = pygame.Rect(x * size[0]+1, y * size[1]+1, size[0]-2, size[1]-2)
         color = (int(value*255), int(value*255), int(value*255))
         pygame.draw.rect(SCREEN, WHITE, bg_rect)
         pygame.draw.rect(SCREEN, color, rect)
 
-
+def deres(factor, grid):
+    new_grid = {}
+    averages = {}
+    for x in range(dimensions[0]):
+        for y in range(dimensions[1]):
+            average = 0
+            for z in range(factor):
+                for a in range(factor):
+                    average += grid.get((x+z, y+a), 0)
+            average = average/factor**2
+            averages[(x, y)] = average
+    for x in range(0, dimensions[0], factor):
+        for y in range(0, dimensions[0], factor):
+            for z in range(factor):
+                for a in range(factor):
+                    new_grid[(x+z, y+a)] = averages[(x+z, y+a)]
+    return new_grid
 
 def runRender():
+    global grid
+    global blurred
+    #print("Hi")
     drawGrid(screen_width, screen_height-100)
     while True:
 
@@ -90,4 +114,10 @@ def runRender():
         if mouse.get_pressed()[2]:
             mouse_pos = mouse.get_pos()
             setBox(mouse_pos, BLACK)
+        
+        if pygame.key.get_pressed()[pygame.K_SPACE] and not blurred:
+            blurred = True
+            grid = deres(3, grid)
+            colorGrid()
+
         pygame.display.update()
