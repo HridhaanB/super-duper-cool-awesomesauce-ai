7bde0c5b0cb3c001de2d265b7fdb650f4d5765af
monroe <jmonroeh44@gmail.com>
2025-07-16 12:06:54 -0400

    cool effect, not anti-aliasing tho :(

--- UI.py
+++ UI.py
@@ -1,52 +1,78 @@
 import pygame, sys
 from pygame.locals import QUIT
 
-
 pygame.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 screen_width = 700
-screen_height = 700
+screen_height = 800
 dimensions = (28, 28)
 
 SCREEN = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('super duper cool awesomesauce ai')
-SCREEN.fill((255, 255, 255))
+SCREEN.fill(BLACK)
 
 mouse = pygame.mouse
 clock = pygame.time.Clock()
 
-grid = {(x, y):False for x in range(dimensions[0]) for y in range(dimensions[1])}
+grid = {(x, y): 0.0 for x in range(dimensions[0]) for y in range(dimensions[1])}
 
-def drawGrid(w, h, d = dimensions):
-    size = (int(w/d[0]), int(h/d[1]))
+
+def drawGrid(w, h, d=dimensions):
+    size = (int(w / d[0]), int(h / d[1]))
     for x in range(0, w, size[0]):
         for y in range(0, h, size[1]):
             rect = pygame.Rect(x, y, size[0], size[1])
-            pygame.draw.rect(SCREEN, (0,0,0), rect, 1)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
-def setBox(p, color, d=dimensions, w=screen_width, h=screen_height):
+
+def setBox(p, color, d=dimensions, w=screen_width, h=screen_height-100):
     x, y = p
-    size = (int(w/d[0]), int(h/d[1]))
-    boxx, boxy = ( int(x/size[0]), int(y/size[1]) )
-    if color != WHITE:
-        if grid[(boxx, boxy)]:
+    size = (int(w / d[0]), int(h / d[1]))
+    boxx, boxy = (int(x / size[0]), int(y / size[1]))
+    if color != BLACK:
+        if grid[(boxx, boxy)] == 1:
             return None
-        grid[( boxx, boxy )] = True
-        rect = pygame.Rect(boxx*size[0], boxy*size[1], size[0], size[1])
+        grid[(boxx, boxy)] = 1
+        anti_alias()
+        color_grid()
     else:
-        grid[( boxx, boxy )] = False
-        rect = pygame.Rect(boxx*size[0]+1, boxy*size[1]+1, size[0] - 2, size[1] - 2)
-    pygame.draw.rect(SCREEN, color, rect)
-    #drawGrid(w, h)
+        #erase
+        grid[(boxx, boxy)] = 0
+    # drawGrid(w, h)
+
+def anti_alias():
+    for key, value in grid.items():
+        x, y = key
+        surrounding_vals = 0.80*value
+        surrounding_vals += 0.04*grid.get((x+1, y), 0)
+        surrounding_vals += 0.04*grid.get((x-1, y), 0)
+        surrounding_vals += 0.04*grid.get((x, y+1), 0)
+        surrounding_vals += 0.04*grid.get((x, y-1), 0)
+        surrounding_vals += 0.01*grid.get((x+1, y+1), 0)
+        surrounding_vals += 0.01*grid.get((x-1, y+1), 0)
+        surrounding_vals += 0.01*grid.get((x+1, y-1), 0)
+        surrounding_vals += 0.01*grid.get((x-1, y-1), 0)
+        grid[key] = surrounding_vals
+
+def color_grid():
+    for key, value in grid.items():
+        x, y = key
+        size = (int(screen_width / 28), int((screen_height-100) / 28))
+        bg_rect = pygame.Rect(x * size[0], y * size[1], size[0], size[1])
+        rect = pygame.Rect(x * size[0]+1, y * size[1]+1, size[0]-2, size[1]-2)
+        color = (int(value*255), int(value*255), int(value*255))
+        pygame.draw.rect(SCREEN, WHITE, bg_rect)
+        pygame.draw.rect(SCREEN, color, rect)
+
 
 
 def runRender():
-    drawGrid(screen_width, screen_height)
+    drawGrid(screen_width, screen_height-100)
     while True:
-        
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -54,8 +80,8 @@ def runRender():
 
         if mouse.get_pressed()[0]:
             mouse_pos = mouse.get_pos()
-            setBox(mouse_pos, BLACK)
+            setBox(mouse_pos, WHITE)
         if mouse.get_pressed()[2]:
             mouse_pos = mouse.get_pos()
-            setBox(mouse_pos, WHITE)
+            setBox(mouse_pos, BLACK)
         pygame.display.update()
