3050d543ea847f2eb24beae28371e072bfb7210c
monroe <jmonroeh44@gmail.com>
2025-07-16 15:41:23 -0400

    moved important files to folder and added the ability to clear and also fixed the bug where the window would close and give an error if the mouse went off screen or touched the bottom part

---
+++ Neural Network/NN.py
@@ -0,0 +1,43 @@
+from keras.datasets import mnist
+from matplotlib import pyplot
+import numpy as np
+import random
+
+print("a")
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+for i in range(9):  
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+    print(type(train_X[i]))
+pyplot.show()
+
+def ReLU(x):
+    if x>=0:
+        return x
+    return 0
+
+class Layer:
+    def __innit__(self, size, connections, prev_layer):
+        self.size = size
+        self.biasVector = vertify([0]*size)
+        self.postmatrix = np.array([[random.random() for a in range(prev_layer.size)] for b in range(size)])
+        self.previousLayer = prev_layer
+        self.nextLayer = None
+    def setActNext(self, actfunc=ReLU):
+        self.nextLayer.setAct(actfunc(self.postmatrix.dot(self.actVector) + self.nextLayer.biasVector))
+    def setAct(self, activation):
+        self.actVector = activation
+    def setNext(self, next):
+        self.nextLayer = next
+        next.previousLayer = self
+
+def NNout(inLay, Grid):
+    inLay.setAct(vertify([Grid[x] for x in Grid]))
+    while inLay.nextLayer!=None:
+        inLay.setActNext()
+        inLay = inLay.nextLayer
+    return inLay.actVector()
+
+def vertify(L):
+    return np.array([[x] for x in L])
---
+++ Neural Network/UI.py
@@ -0,0 +1,100 @@
+import pygame, sys
+from pygame.locals import QUIT
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen_width = 700
+screen_height = 800
+dimensions = (28, 28)
+
+SCREEN = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('super duper cool awesomesauce ai')
+SCREEN.fill(BLACK)
+
+mouse = pygame.mouse
+clock = pygame.time.Clock()
+
+grid = {(x, y): 0.0 for x in range(dimensions[0]) for y in range(dimensions[1])}
+
+
+def drawGrid(w, h, d=dimensions):
+    size = (int(w / d[0]), int(h / d[1]))
+    for x in range(0, w, size[0]):
+        for y in range(0, h, size[1]):
+            rect = pygame.Rect(x, y, size[0], size[1])
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+
+def setBox(p, color, d=dimensions, w=screen_width, h=screen_height-100):
+    boxx, boxy = mousePosToGrid(p)
+    if color != BLACK:
+        if grid.get((boxx, boxy), 1) == 1:
+            return None
+        grid[(boxx, boxy)] = 1
+        antiAlias(p)
+        #colorGrid()
+    else:
+        #erase
+        grid[(boxx, boxy)] = 0
+        #colorGrid()
+    # drawGrid(w, h)
+
+def mousePosToGrid(pos):
+    x, y = pos
+    size = int(screen_width / dimensions[0]), int((screen_height-100) / dimensions[1])
+    return int(x / size[0]), int(y / size[1])
+
+def antiAlias(pos):
+    x, y = mousePosToGrid(pos)
+    for x_pos in range(x-1, x+1):
+        for y_pos in range(y-1, y+1):
+            surrounding_vals = 0.7*grid.get((x_pos, y_pos), 0)
+            surrounding_vals += 0.05*grid.get((x_pos+1, y_pos), 0)
+            surrounding_vals += 0.05*grid.get((x_pos-1, y_pos), 0)
+            surrounding_vals += 0.05*grid.get((x_pos, y_pos+1), 0)
+            surrounding_vals += 0.05*grid.get((x_pos, y_pos-1), 0)
+            surrounding_vals += 0.0125*grid.get((x_pos+1, y_pos+1), 0)
+            surrounding_vals += 0.0125*grid.get((x_pos-1, y_pos+1), 0)
+            surrounding_vals += 0.0125*grid.get((x_pos+1, y_pos-1), 0)
+            surrounding_vals += 0.0125*grid.get((x_pos-1, y_pos-1), 0)
+            if surrounding_vals >= grid.get((x_pos, y_pos), 0):
+                grid[x_pos, y_pos] = surrounding_vals
+
+def colorGrid():
+    for key, value in grid.items():
+        x, y = key
+        size = (int(screen_width / 28), int((screen_height-100) / 28))
+        bg_rect = pygame.Rect(x * size[0], y * size[1], size[0], size[1])
+        rect = pygame.Rect(x * size[0]+1, y * size[1]+1, size[0]-2, size[1]-2)
+        color = (int(value*255), int(value*255), int(value*255))
+        pygame.draw.rect(SCREEN, WHITE, bg_rect)
+        pygame.draw.rect(SCREEN, color, rect)
+
+
+
+def runRender():
+    global grid
+
+    drawGrid(screen_width, screen_height-100)
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            grid = {(x, y): 0.0 for x in range(dimensions[0]) for y in range(dimensions[1])}
+
+        if mouse.get_pressed()[0]:
+            mouse_pos = mouse.get_pos()
+            setBox(mouse_pos, WHITE)
+        if mouse.get_pressed()[2]:
+            mouse_pos = mouse.get_pos()
+            setBox(mouse_pos, BLACK)
+
+        colorGrid()
+        pygame.display.update()
