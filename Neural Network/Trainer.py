7dd39df90bf6fa0e92f6ec5203364db401b095a6
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-07-17 12:13:38 -0400

    Update Trainer.py
    Actually training stuf?

--- Neural Network/Trainer.py
+++ Neural Network/Trainer.py
@@ -1,19 +1,8 @@
 from keras.datasets import mnist
 from matplotlib import pyplot
 import pickle
-import NN
-print("a")
-(train_X, train_y), (test_X, test_y) = mnist.load_data()
+from NN import *
 
-print(train_X[1])
-
-#set in layer
-inlayer = []
-
-wb = NN.getWeightsAndBiases(inlayer)
-
-wbfile = open('weights_and_biases', 'ab')
-
-# source, destination
-pickle.dump(wb, wbfile)
-wbfile.close()
+for win in range(100):
+    update(get_averaged_gradient(output_layer, input_layer, win), input_layer)
+print(getStuff(input_layer))
