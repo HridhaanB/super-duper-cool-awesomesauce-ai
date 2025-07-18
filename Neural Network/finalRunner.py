3e5dbf25fda78435cdbcbe6f59adf51894742eb6
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-07-18 09:34:08 -0400

    Update finalRunner.py

--- Neural Network/finalRunner.py
+++ Neural Network/finalRunner.py
@@ -1,3 +1,11 @@
 import UI
+import NN
+import numpy as np
 
-UI.runRender()
+x = UI.runRender()
+
+z = NN.NNout([[x] for y in x.values()])
+
+answer = np.where( z == z.max() )
+
+print(answer[0][0])
